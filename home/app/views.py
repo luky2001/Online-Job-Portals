d713aabd4ee6b9472c78a000ecd2d60b5c91e713
Rohit Patel <luckykumarpatel2001@gmail.com>
2025-04-23 21:51:05 +0530

    main function

--- home/app/views.py
+++ home/app/views.py
@@ -235,4 +235,4 @@ def candidate_update_profile(request,id):
         queryset.save()
         return redirect("/show_candidate_profile/")
     return render(request, 'candidate_profile_update.html', {'candidate': queryset})
-def
+def application
